218c835d4a1583d59204bb437bfad077d60f4266
Josh Watts <josh+github@sroz.net>
2016-11-22 14:42:56 -0500

    Remove search related features

--- options.js
+++ options.js
@@ -7,17 +7,12 @@ function save_options() {
 	var showApps = document.getElementById('apps').checked;
 	var showTop = document.getElementById('top').checked;
 
-	var searche = document.getElementById('search').value;
-	var searchplace = document.getElementById('searchplace').value;
-
 	chrome.storage.sync.set({
-	"search": searche,
 	"l1name": l1name,
 	"l2name": l2name,
 	"l3name": l3name,
 	"showApps": showApps,
 	"showTop": showTop,
-	"searchplace": searchplace
 	}, function() {
 	// Update status to let user know options were saved.
 	var status = document.getElementById('status');
@@ -33,21 +28,17 @@ function save_options() {
 function restore_options() {
   // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
-	"search": "google",
   "l1name": "productivity",
 	"l2name": "subreddits",
 	"l3name": "#222222",
 	"showApps": true,
 	"showTop": true,
-	"searchplace": "bottom"
   }, function(items) {
-	document.getElementById('search').value = items["search"];
 	document.getElementById('l1').value = items["l1name"];
 	document.getElementById('l2').value = items["l2name"];
 	document.getElementById('l3').value = items["l3name"];
 	document.getElementById('apps').checked = items["showApps"];
   document.getElementById('top').checked = items["showTop"];
-	document.getElementById('searchplace').value = items["searchplace"];
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
